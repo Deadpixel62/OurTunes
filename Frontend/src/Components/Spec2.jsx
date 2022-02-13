4cc7d4341049d3d39e0e5e1622f367847681b3d3
Sky Tech <bouhaouche.med@hotmail.com>
2022-02-13 02:38:05 +0100

    wteaking layout style

--- Frontend/src/Components/Spec2.jsx
+++ Frontend/src/Components/Spec2.jsx
@@ -61,7 +61,7 @@ const dispatch = useDispatch();
 
   return (
       
-    <Row style={{justifyContent:"center"}}  gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+    <Row style={{justifyContent:"center", gap: "6vh"}}  gutter={{ xs: 8 }}>
 
    {filteredData.map((card)=> {
        return (
@@ -70,7 +70,7 @@ const dispatch = useDispatch();
            className="gutter-row "
            span={6}
            key={card.id}
-           style={{ width: 300, margin: 10 }}
+           style={{ width: 300}}
            cover={
              <img
                style={{ height: "200px"  }}
